318edc85f714a852ea037757b8fd15702009fbed
trsaints <thiago.dev2k3@gmail.com>
2024-10-25 18:38:46 -0300

    feat: load products upon component render

--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -1,16 +1,20 @@
-import { ComponentProps } from 'react'
 import { Card } from '../components/Card.tsx'
+import { useEffect } from 'react'
+import { ProductService } from '../data/services/product.service.ts'
 
 
 export { Home }
 
-interface IHome extends ComponentProps<'article'> {
-}
+function Home() {
+	useEffect(() => {
+		ProductService.getAll().then(products => {
+			console.log(products)
+		})
+	}, [])
 
-function Home(props: IHome) {
 	return (
-		<article className='px-10'>
-			<h1 className='mb-24 text-3xl font-bold'>TIC Market</h1>
+		<article className="px-10">
+			<h1 className="mb-24 text-3xl font-bold">TIC Market</h1>
 
 			<Card banner="https://picsum.photos/200" name="Tainha"
 				  value={29.99}/>
