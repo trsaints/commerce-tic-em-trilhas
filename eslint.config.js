544cb67376ff07410349bbf87f0a1e6f174d4a70
trsaints <thiago.dev2k3@gmail.com>
2024-10-25 10:13:04 -0300

    raw: add react-refresh plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,36 @@
-import js from '@eslint/js'
+import reactHooks from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import reactRefresh from 'react-refresh'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+export default [
+	{
+		ignores        : ['dist'],
+		extends        : [
+			'plugin:@eslint/js/recommended',
+			'plugin:@typescript-eslint/recommended',
+			'plugin:@typescript-eslint/recommended-type-checked',
+			'plugin:@typescript-eslint/stylistic-type-checked',
+			'plugin:react/recommended',
+			'plugin:react/jsx-runtime',
+			'plugin:react-hooks/recommended'
+		],
+		files          : ['**/*.{ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			project: ['tsconfig.json', 'tsconfig.node.json'],
+			globals    : globals.browser
+		},
+		plugins        : {
+			'react-hooks'  : reactHooks,
+			'react-refresh': reactRefresh
+		},
+		rules          : {
+			...reactHooks.configs.recommended.rules,
+			'react-refresh/only-export-components': [
+				'warn',
+				{ allowConstantExport: true }
+			]
+		}
+	}
+]
