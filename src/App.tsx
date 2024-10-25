fda43a3476432cea674026a7303d08320c7c82b7
trsaints <thiago.dev2k3@gmail.com>
2024-10-25 12:02:56 -0300

    feat: add base skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Header } from './components/Header.tsx'
 
+
+const mainRouter = createBrowserRouter([
+										   {
+											   path   : '/',
+											   element: <Header/>,
+											   index  : true
+										   }
+									   ])
 
 export function App() {
 	return (
 		<main
 			className="flex justify-content-center items-center bg-gray-200 h-screen">
-			<Header/>
+			<RouterProvider router={mainRouter}/>
 		</main>
 	)
 }
